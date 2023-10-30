f6345f7d38d5b23fef4711e77194f4f4aee577d7
ManishKaswan05 <manishkaswan888@gmail.com>
2023-10-30 20:59:40 +0530

    new file

---
+++ DOM/dom.js
@@ -0,0 +1,6 @@
+document.getElementById("main-header").style.borderBottom = "solid 3px #000";
+document.getElementsByClassName("title")[0].style.color = "green";
+document.getElementsByClassName("list-group-item")[2].style.background = "green"
+document.getElementsByClassName("list-group-item")[0].style.fontWeight = "bold"
+document.getElementsByClassName("list-group-item")[1].style.fontWeight = "bold"
+document.getElementsByClassName("list-group-item")[3].style.fontWeight = "bold"
