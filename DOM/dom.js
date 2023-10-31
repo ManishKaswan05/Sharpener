557133f62017a176348ae42197a251efa81978fe
ManishKaswan05 <manishkaswan888@gmail.com>
2023-10-31 12:48:04 +0530

    changes some

--- DOM/dom.js
+++ DOM/dom.js
@@ -4,3 +4,14 @@ document.getElementsByClassName("list-group-item")[2].style.background = "green"
 document.getElementsByClassName("list-group-item")[0].style.fontWeight = "bold"
 document.getElementsByClassName("list-group-item")[1].style.fontWeight = "bold"
 document.getElementsByClassName("list-group-item")[3].style.fontWeight = "bold"
+// document.querySelectorAll("li")[1].style.backgroundColor = "green"
+// document.querySelectorAll("li")[2].style.display = "None"
+// document.querySelectorAll("li")[1].style.color = "green"
+document.querySelectorAll("li")[1].style.backgroundColor = "green"
+document.querySelectorAll("li")[3].style.backgroundColor = "green"
+
+
+
+
+
+
